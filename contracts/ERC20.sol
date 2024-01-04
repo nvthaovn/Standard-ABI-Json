ccda880608a5d254c98fd31d55959d1a77850a78
Thao <nvthaovn@users.noreply.github.com>
2024-01-04 21:56:00 +0700

    Add files via upload

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NewToken is ERC20 {
-  
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {}
 }
